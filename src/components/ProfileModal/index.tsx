5cc84972cab1cf5f16914068bc88d63c4b929ef8
Matheus Lima Machado <matmachado@uolinc.com>
2022-03-23 23:11:26 -0400

    feat: displays number of posts by username.

--- src/components/ProfileModal/index.tsx
+++ src/components/ProfileModal/index.tsx
@@ -1,4 +1,6 @@
+import { usePosts } from 'hooks/use-posts'
 import { useProfileModal } from 'hooks/use-profile-modal'
+import { useEffect, useState } from 'react'
 
 import Modal from 'react-modal'
 
@@ -13,10 +15,29 @@ const customStyles = {
   },
 }
 
+// TODO: put this type definition in a shared space.
+type PostType = {
+  id: number
+  author: {
+    name: string
+    username: string
+  }
+  postContent: string
+}
+
 Modal.setAppElement('#root')
 
 const ProfileModal = () => {
+  const [numberOfPosts, setNumberOfPosts] = useState(0)
   const { user, isShown, closeModal } = useProfileModal()
+  const { allPosts } = usePosts()
+
+  useEffect(() => {
+    const postsFromUser = allPosts.filter(
+      (post: PostType) => post.author.username === user.username
+    )
+    setNumberOfPosts(postsFromUser.length)
+  }, [allPosts, user.username])
 
   return (
     <Modal
@@ -30,7 +51,7 @@ const ProfileModal = () => {
       {'Joined ' + user.joiningData + ' | '}
       {user.following.length + ' Following | '}
       {user.followers.length + ' Followers '}
-      <button onClick={closeModal}>close</button>
+      {numberOfPosts + 'Posts'}
     </Modal>
   )
 }
